b59fd96fa75cb6883b11d49c34345f3187746347
leehayford <leehayford@gmail.com>
2021-01-31 08:20:48 -0700

    Initial commit  - Flask - SocketIO - before svelte

---
+++ src/app.py
@@ -0,0 +1,106 @@
+from flask import Flask, render_template # ~/$ pip install flask
+from flask_socketio import SocketIO, send, emit #~/$ pip install flask-socketio
+from datetime import timedelta
+import json
+import time
+import threading
+
+app = Flask(__name__)
+app.config["SEND_FILE_MAX_AGE_DEFAULT"] = timedelta(seconds=1) # DEBUG ONLY - avoid cached shite confusion during development
+app.config["SECRET_KEY"] = 'secrets!'
+sio = SocketIO(app)
+
+class LData:
+	def __init__( self,
+	depth = 0.0,
+	tension = 0.0,
+	speed = 0.0,
+	ccl = 0.0,
+	gr = 0.0,
+	temp = 0.0,
+	press = 0.0,
+	flowUp = 0.0,
+	flowDn = 0.0,
+	diffPress = 0.0,
+	spinner = 0.0
+	):
+		self.depth = depth
+		self.tension = tension
+		self.speed = speed
+		self.ccl = ccl
+		self.gr = gr
+		self.temp = temp
+		self.press = press
+		self.flowUp = flowUp
+		self.flowDn = flowDn
+		self.diffPress = diffPress
+		self.spinner = spinner
+
+count = 0
+def sendsimdata( count ):
+	f = open("pl-data-short.txt", "r")
+	f.readline()
+	for line in f:
+		l = str(line.encode('utf-8').strip()).split("'")[1]
+		d = LData( )
+		d.depth = float(l.split()[0])
+		d.tension =	float(l.split()[1])
+		d.speed	= float(l.split()[2])
+		d.ccl =	float(l.split()[3])
+		d.gr =	float(l.split()[4])
+		d.temp =	float(l.split()[5])
+		d.press =	float(l.split()[6])
+		d.flowUp =	float(l.split()[7])
+		d.flowDn =	float(l.split()[8])
+		d.diffPress = float(l.split()[9])
+		d.spinner =	float(l.split()[10])
+		djson= json.dumps(d.__dict__)
+		print(djson)
+		emit('newdata', djson )
+		time.sleep(0.2)
+		count += 1
+	f.close()
+	print(f'\n[*** SENT DATA ***]\t {count} samples @ 20 / second...\n')
+
+@sio.on('getsimdata')
+def handle_getsimdata( msg ):
+	print(f'\n[*** DATA REQUESTED ***]\n{ msg }\n')
+	x = threading.Thread(target=sendsimdata( count ))
+	x.start()
+	x.join()
+
+@app.route("/")
+def home():
+	return render_template("home.html", title="Home")
+
+@app.route("/data")
+def datago( ):
+	f = open("pl-data.txt", "r")
+	f.readline()
+	l = str(f.readline().encode('utf-8').strip()).split("'")[1]
+	data = LData(
+		float(l.split()[0]),	#depth
+		float(l.split()[1]),	# tension
+		float(l.split()[2]),	#speed
+		float(l.split()[3]),	#ccl
+		float(l.split()[4]),	# gr
+		float(l.split()[5]),	# temp
+		float(l.split()[6]),	#press
+		float(l.split()[7]),	#flowUp
+		float(l.split()[8]),	# flowDn
+		float(l.split()[9]),	# diffPress
+		float(l.split()[10])	#spinner
+	)
+	print(data.__dict__)
+	f.close()
+	return render_template("data.html", data=data, title='Data')
+
+if __name__ == "__main__":
+	# app.run(debug=True)
+	sio.run(app)
+
+### Go Buttons ##
+# ~/$ sourece bin/activate
+# ~/$ cd src
+# ~/src/$ export FLASK_APP=app.py
+# ~/src/$ flask run
