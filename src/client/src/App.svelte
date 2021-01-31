fff02a9726870c210d2438f56564a74516c9ddf1
leehayford <leehayford@gmail.com>
2021-01-31 09:47:02 -0700

    flask back-end  is  serving - svelte front is  receiving

--- src/client/src/App.svelte
+++ src/client/src/App.svelte
@@ -1,11 +1,49 @@
 <script>
-	export let name;
+	// export let name;
+	export let depth;
+	export let tension;
+	export let speed;
+	export let ccl;
+	export let gr;
+	export let temp;
+	export let press;
+	export let flowUp;
+	export let flowDn;
+	export let spinner;
+
+	var socket = io();
+	socket.on('connect', function() {
+		console.log('\n[*** CONNECTED ***]\nconnected - svelte')
+		socket.emit('getsimdata',  'svelte client ready for data....\n' );
+	});
+	socket.on('newdata', function(djson) {
+		var d = JSON.parse(djson);
+		// console.log('\n[*** NEW DATA RECEIVED ***]\n', d['depth'])
+		depth = d['depth'];
+		tension = d['tension'];
+		speed = d['speed'];
+		ccl = d['ccl'];
+		gr = d['gr'];
+		temp = d['temp'];
+		press = d['press'];
+		flowUp = d['flowUp'];
+		flowDn = d['flowDn'];
+		spinner = d['spinner'];
+	});
 </script>
 
 <main>
-	<!-- <h1>Hello {name}!</h1> -->
-	<!-- <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p> -->
 	<h1>Sup?</h1>
+	<p>depth: {depth}</p>
+	<p>tension: {tension}</p>
+	<p>speed: {speed}</p>
+	<p>ccl: {ccl}</p>
+	<p>gr: {gr}</p>
+	<p>temp: {temp}</p>
+	<p>press: {press}</p>
+	<p>flowUp: {flowUp}</p>
+	<p>flowDn: {flowDn}</p>
+	<p>spinner: {spinner}</p>
 </main>
 
 <style>
--- src/client/src/main.js
+++ src/client/src/main.js
@@ -3,7 +3,17 @@ import App from './App.svelte';
 const app = new App({
 	target: document.body,
 	props: {
-		name: 'world'
+		depth: 0.0,
+		tension: 0.0,
+		speed: 0.0,
+		ccl: 0.0,
+		gr: 0.0,
+		temp: 0.0,
+		press: 0.0,
+		flowUp: 0.0,
+		flowDn: 0.0,
+		diffPress: 0.0,
+		spinner: 0.0
 	}
 });
 
